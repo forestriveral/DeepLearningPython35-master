39a74fcd7e716794b11771861fbe38f9c7506d76
forestriveral <forestriveral@gmail.com>
2018-08-06 21:19:24 +0800

    convolution

--- network_run.py
+++ network_run.py
@@ -10,14 +10,14 @@ training_data = list(training_data)
 
 # ========================================network
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+# net_1 = network.Network([784, 30, 10])
+# net_1.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 # ========================================network2
 
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-#net.large_weight_initializer()
-net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data,
+net_2 = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net_2.large_weight_initializer()
+net_2.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data,
     monitor_evaluation_accuracy=True)
 
 # ========================================network3
