2d663885d5adaf46522aaf3aed3004bc7d44b280
forestriveral <forestriveral@gmail.com>
2018-08-09 19:45:12 +0800

    CS231n more

---
+++ mnist/network_run.py
@@ -0,0 +1,23 @@
+from mnist import mnist_loader, network2
+
+# ========================================read data
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+
+# ========================================network
+
+# net_1 = network.Network([784, 30, 10])
+# net_1.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# ========================================network2
+
+net_2 = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net_2.large_weight_initializer()
+net_2.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data,
+    monitor_evaluation_accuracy=True)
+
+# ========================================network3
+
+
+
+
